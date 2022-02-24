f870c62cdd870869c8165009d3de66e86a8c1c73
Severus7 <joshuasapz@gmail.com>
2022-02-24 22:32:53 +0800

    added theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material";
 import {
   AppBar,
   Box,
@@ -9,31 +10,34 @@ import {
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
 import ImageCard from "./components/ImageCard";
+import customTheme from "./assets/theme";
 
 const App = () => {
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <Menu />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <ImageCard />
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <Menu />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <ImageCard />
+      </div>
+    </ThemeProvider>
   );
 };
 
--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,1 +1,41 @@
 import { createTheme } from "@mui/material/styles";
+import { purple, orange, red } from "@mui/material/colors";
+
+const primaryColor = purple[600];
+const secondaryColor = orange[500];
+const dangerColor = red[900];
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: secondaryColor,
+    },
+    error: {
+      main: dangerColor,
+    },
+  },
+  components: {
+    MuiFab: {
+      styleOverrides: {
+        secondary: {
+          "&:hover": {
+            color: "white",
+          },
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          background: dangerColor,
+          marginBottom: 50,
+        },
+      },
+    },
+  },
+});
+
+export default customTheme;
